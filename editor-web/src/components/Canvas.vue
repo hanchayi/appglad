e2ca69de371f9c0022dff665a6609dc949776481
zhangyi <zhangyi@corp-ci.com>
2022-11-15 16:32:50 +0800

    feat: canvas render

--- editor-web/src/components/Canvas.vue
+++ editor-web/src/components/Canvas.vue
@@ -67,41 +67,32 @@ function loadShader(gl: WebGLRenderingContext, type: number, source: string) {
   return shader;
 }
 
-function initBuffers(gl: WebGLRenderingContext): Buffers {
+function initBuffers(gl: WebGLRenderingContext) {
   // Create a buffer for the square's positions.
+
   const positionBuffer = gl.createBuffer();
 
-  if (!positionBuffer) {
-    throw new Error('create Buffer error')
-  }
   // Select the positionBuffer as the one to apply buffer
   // operations to from here out.
+
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
   // Now create an array of positions for the square.
-  const positions = [
-    1.0,  1.0,
-    -1.0,  1.0,
-    1.0, -1.0,
-    -1.0, -1.0,
-  ];
+
+  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
+
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
 
-  gl.bufferData(gl.ARRAY_BUFFER,
-                new Float32Array(positions),
-                gl.STATIC_DRAW);
-
-  return {
-    position: positionBuffer,
-  };
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 }
 
-function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers: Buffers) {
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
-  gl.clearDepth(1.0);                 // Clear everything
-  gl.enable(gl.DEPTH_TEST);           // Enable depth testing
-  gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
+function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo) {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0); // Clear to black, fully opaque
+  gl.clearDepth(1.0); // Clear everything
+  gl.enable(gl.DEPTH_TEST); // Enable depth testing
+  gl.depthFunc(gl.LEQUAL); // Near things obscure far things
 
   // Clear the canvas before we start drawing on it.
 
@@ -114,7 +105,7 @@ function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
   // and we only want to see objects between 0.1 units
   // and 100 units away from the camera.
 
-  const fieldOfView = 45 * Math.PI / 180;   // in radians
+  const fieldOfView = (45 * Math.PI) / 180; // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
@@ -122,11 +113,7 @@ function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
 
   // note: glmatrix.js always has the first argument
   // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
-                  fieldOfView,
-                  aspect,
-                  zNear,
-                  zFar);
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
 
   // Set the drawing position to the "identity" point, which is
   // the center of the scene.
@@ -135,29 +122,29 @@ function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
   // Now move the drawing position a bit to where we want to
   // start drawing the square.
 
-  mat4.translate(modelViewMatrix,     // destination matrix
-                modelViewMatrix,     // matrix to translate
-                [-0.0, 0.0, -6.0]);  // amount to translate
+  mat4.translate(
+    modelViewMatrix, // destination matrix
+    modelViewMatrix, // matrix to translate
+    [-0.0, 0.0, -6.0]
+  ); // amount to translate
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   {
-    const numComponents = 3;  // pull out 3 values per iteration
-    const type = gl.FLOAT;    // the data in the buffer is 32bit floats
-    const normalize = false;  // don't normalize
-    const stride = 0;         // how many bytes to get from one set of values to the next
-                              // 0 = use type and numComponents above
-    const offset = 0;         // how many bytes inside the buffer to start from
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
+    const numComponents = 2;
+    const type = gl.FLOAT;
+    const normalize = false;
+    const stride = 0;
+    const offset = 0;
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
-    gl.enableVertexAttribArray(
-        programInfo.attribLocations.vertexPosition);
+      programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
+    gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
 
   // Tell WebGL to use our program when drawing
@@ -167,13 +154,15 @@ function drawScene(gl: WebGLRenderingContext, programInfo: ProgramInfo, buffers:
   // Set the shader uniforms
 
   gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
+    programInfo.uniformLocations.projectionMatrix,
+    false,
+    projectionMatrix
+  );
   gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
+    programInfo.uniformLocations.modelViewMatrix,
+    false,
+    modelViewMatrix
+  );
 
   {
     const offset = 0;
@@ -243,12 +232,12 @@ onMounted(() => {
     },
   };
 
-  const buffers = initBuffers(gl);
-  drawScene(gl, programInfo, buffers);  
+  initBuffers(gl);
+  drawScene(gl, programInfo);  
 })
 </script>
 
 <template>
-  <canvas ref='canvas'/>
+  <canvas ref='canvas' width="640" height="480"/>
 </template>
 
