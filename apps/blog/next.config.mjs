fea154368e0d5022c8c7e93a310bd7d802397edd
hanchayi <hanchayi@163.com>
2023-05-10 16:23:51 +0800

    feat: change to docs theme

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -2,12 +2,12 @@ import { remarkMermaid } from 'remark-mermaid-nextra';
 import nextra from 'nextra'
 
 const withNextra = nextra({
-  theme: 'nextra-theme-blog',
+  theme: 'nextra-theme-docs',
   latex: true,
   themeConfig: './theme.config.jsx',
   mdxOptions: {
     remarkPlugins: [remarkMermaid],
-}
+  }
 })
 
 export default withNextra()
---
+++ apps/blog/theme.config.jsx
@@ -0,0 +1,29 @@
+export default {
+  logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra',
+  },
+  // footer: <a href="https://beian.miit.gov.cn">苏ICP备2022043623号</a>,
+  // head: ({ title, meta }) => (
+  //   <>
+  //     {meta.description && (
+  //       <meta name="description" content={meta.description} />
+  //     )}
+  //     {meta.tag && <meta name="keywords" content={meta.tag} />}
+  //     {meta.author && <meta name="author" content={meta.author} />}
+  //   </>
+  // ),
+  // readMore: 'Read More →',
+  // postFooter: null,
+  // darkMode: false,
+  // navs: [
+  //   {
+  //     url: 'https://github.com/shuding/nextra',
+  //     name: 'Nextra'
+  //   },
+  //   {
+  //     url: 'https://github.com/shuding/nextra',
+  //     name: 'Nextra'
+  //   }
+  // ]
+}
